16e59cc7b564c02cc30027034bc64dd5282cad45
Nicolas <saz12gaming@gmail.com>
2025-02-07 15:29:13 +0100

    Update funct.py

--- funct.py
+++ funct.py
@@ -11,24 +11,16 @@ def func_type(func: str, harm: int) -> None:
         case "scie":
             scie(harm)
 
-def carre(harm: int, nb_point: int = 10000) -> None:
-    array = []
-    for i in range(0,harm):
-        if(i%2)!=0:
-            array += [i]
+def carre(harm: int, nb_point: int = 10000):
 
     x = np.linspace(-8*np.pi, 8*np.pi, nb_point)
     y = np.sin(x)
-    for i in range(1,len(array)-1):
-        y += np.sin(x * array[i])/array[i]
 
-    #flèche pour T
-    plt.arrow(2*np.pi,-0.5,0,0.5)
-    plt.text(2*np.pi,-0.5,"T")
-
-    #flèche pour T/2
-    plt.arrow(np.pi,-0.5,0,0.5)
-    plt.text(np.pi,-0.5,"T/2")
+    if harm != 0:
+        y = y * 0
+        for i in range(0,harm):
+            y += np.sin(x * (2*i+1))/(2*i+1)
+        y *= 4/np.pi
 
     plt.title(f"Fonction carré à {harm} harmonique(s)")
     plt.plot(x,y)
@@ -36,30 +28,15 @@ def carre(harm: int, nb_point: int = 10000) -> None:
     plt.grid(True)
     plt.show()
 
-def triangle(harm: int, nb_point: int = 10000) -> None:
-    array = []
-    val = True
-    for i in range(0,harm):
-        if(i%2)!=0:
-            array += [i]
-
+def triangle(harm: int, nb_point: int = 10000):
     x = np.linspace(-8*np.pi,8*np.pi, nb_point)
     y = np.sin(x)
-    for i in range(1,len(array)-1):
-        if val:
-            y -= np.sin(x * array[i])/(array[i]**2)
-            val = False
-        elif not val:
-            y += np.sin(x * array[i])/(array[i]**2)
-            val = True
-
-    #flèche pour T
-    plt.arrow(2*np.pi,-0.5,0,0.5)
-    plt.text(2*np.pi,-0.5,"T")
-
-    #flèche pour T/2
-    plt.arrow(np.pi,-0.5,0,0.5)
-    plt.text(np.pi,-0.5,"T/2")
+    
+    if harm != 0:
+        y = 0 * y
+        for i in range(0, harm):
+            y += ((-1)**i) * (np.sin(x * (2*i+1))/((2*i+1)**2))
+        y *= 8/np.pi**2
 
     plt.title(f"Fonction triangle à {harm} harmonique(s)")
     plt.plot(x,y)
@@ -67,21 +44,15 @@ def triangle(harm: int, nb_point: int = 10000) -> None:
     plt.grid(True)
     plt.show()
 
-
-def scie(harm: int, nb_point: int = 10000) -> None:
+def scie(harm: int, nb_point: int = 10000):
     x = np.linspace(-8*np.pi, 8*np.pi, nb_point)
     y = np.sin(x)
 
-    for n in range(2, harm + 1):
-        y +=  ((-1)**i+1) * np.sin(x * array[i])/array[i]
-
-    #flèche pour T
-    plt.arrow(2*np.pi,-0.5,0,0.5)
-    plt.text(2*np.pi,-0.5,"T")
-
-    #flèche pour T/2
-    plt.arrow(np.pi,-0.5,0,0.5)
-    plt.text(np.pi,-0.5,"T/2")
+    if harm != 0:
+        y = 0 * y
+        for n in range(1, harm):
+            y += ((-1)**(n+1)) * np.sin(x*n)/n
+        y *= 2/np.pi
 
     plt.title(f"Fonction scie à {harm} harmonique(s)")
     plt.plot(x,y)
